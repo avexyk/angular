943616f1c71791fc038ec67c7efbce819422b4f5
Francisco Javier Gil Bautista <avexyk@gmail.com>
2022-07-15 22:28:23 -0500

    add: obtener referencias a objetos html mediante viewchild

--- s07-gifSearch/src/app/gifs/busqueda/busqueda.component.ts
+++ s07-gifSearch/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
+  // Se pueden usar etiquetas html, clases, id y referencia local || !: operador de que el objeto no es nulo
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    console.log( this.txtBuscar.nativeElement.value );
+    this.txtBuscar.nativeElement.value = "";
   }
 
 }

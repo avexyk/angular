3c91708700bd407086461674f67ce5252468f809
Francisco Javier Gil Bautista <avexyk@gmail.com>
2022-07-21 13:20:39 -0500

    ver-pais: UPDATE subscribe to rxjs switchmap

--- s08-spa/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ s08-spa/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -16,18 +17,31 @@ export class VerPaisComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
+    // rxjs switchmap
     this.activatedRoute.params
-        // desestructuracion
-        .subscribe( ({id}) => {
-          console.log( id );
-
-          this.paisService.getPaisPorAplha( id )
-              .subscribe( pais => {
-                console.log( pais );
-              });
-
+        .pipe(
+          // recibe un observable y retorna un observable
+          switchMap( ({id}) => this.paisService.getPaisPorAplha( id ))
+        )
+        .subscribe( resp => {
+          console.log( resp );
         });
 
+    // #2 Forma
+    // this.activatedRoute.params
+    //     // desestructuracion
+    //     .subscribe( ({id}) => {
+    //       console.log( id );
+
+    //       this.paisService.getPaisPorAplha( id )
+    //           .subscribe( pais => {
+    //             console.log( pais );
+    //           });
+
+    //     });
+
+        // #1 Forma
         // forma tradicional
         // .subscribe(params => {
           // El nombre de la propiedad que sal en consola se encuentra definido en el app-routing-module.ts
